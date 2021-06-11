c6f851c498d0c879452563822549ee2f41112a0a
Samiul karim <samiulkarimprodhan@gmail.com>
2021-06-12 02:08:04 +0600

    added button

--- src/seasonDisplay.js
+++ src/seasonDisplay.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 class SeasonDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.routeToMap = this.routeToMap.bind(this);
+      }
   state = {
-    longitude: null,
-    latitude: null,
+    longitude: '',
+    latitude: '',
     err: "",
   };
   componentDidMount() {
@@ -16,12 +20,14 @@ class SeasonDisplay extends React.Component {
       (error) => this.setState({ err: error })
     );
   }
-
+  routeToMap(){
+      
+  window.location.replace(`https://maps.google.com/?q=${this.state.latitude},${this.state.longitude}`)
+  }
   render() {
     return (
       <div>
-        {this.state.err ? alert("There is an error. Maybe you dont give location permission") :
-  window.location.replace(`https://maps.google.com/?q=${this.state.latitude},${this.state.longitude}`)}
+        {this.state.err ? alert("There is an error. Maybe you dont give location permission") : <button onClick ={this.routeToMap}>Take me to the map</button>}
       </div>
     );
   }
