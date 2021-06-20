7e9723d7c9c9336f6ae065479724dcf1a6d0a80e
Samiul karim <samiulkarimprodhan@gmail.com>
2021-06-20 11:02:31 +0600

    added column with raw css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './seasonDisplay';
+import "./App.css"
+class App extends React.Component {
+  
+state = {
+  latitude: '',
+  err: "",
+};
+componentDidMount() {
+  navigator.geolocation.getCurrentPosition(
+    (success) =>
+      this.setState({
+        latitude: success.coords.latitude,
+      }),
+    (error) => this.setState({ err: error })
+  );
+}
 
-ReactDOM.render(
-  <SeasonDisplay></SeasonDisplay>,
-  document.getElementById('root')
-); 
+render() {
+  return (
+    <div className = "main-div">
+      {this.state.err ? alert("There is an error. Maybe you dont give location permission") : <SeasonDisplay lat = {this.state.latitude}></SeasonDisplay>}
+    </div>
+  );
+}
+}
+ReactDOM.render(<App></App>, document.getElementById("root"));
+
 
--- src/seasonDisplay.js
+++ src/seasonDisplay.js
@@ -1,33 +1,35 @@
 import React from "react";
-
+import "./App.css";
 class SeasonDisplay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.routeToMap = this.routeToMap.bind(this);
-      }
-  state = {
-    longitude: '',
-    latitude: '',
-    err: "",
-  };
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-      (success) =>
-        this.setState({
-          latitude: success.coords.latitude,
-          longitude: success.coords.longitude,
-        }),
-      (error) => this.setState({ err: error })
-    );
-  }
-  routeToMap(){
-      
-  window.location.replace(`https://maps.google.com/?q=${this.state.latitude},${this.state.longitude}`)
-  }
   render() {
     return (
-      <div>
-        {this.state.err ? alert("There is an error. Maybe you dont give location permission") : <button onClick ={this.routeToMap}>Take me to the map</button>}
+      <div className="seasonDisplay">
+        <div className="ui raised very padded text container segment inner_container">
+          <div class="two column stackable ui grid">
+            <div class="column">
+              <div class="ui segment">
+                <p></p>
+                <p></p>
+              </div>
+            </div>
+            <div class="column">
+              <div class="ui segment">
+                <p></p>
+                <p></p>
+              </div>
+            </div>
+          </div>
+          {/* <div className="row">
+            <div className="column right">
+              <h2>Column 1</h2>
+              <p>Some text..</p>
+            </div>
+            <div className="column left">
+              <h2>Column 2</h2>
+              <p>Some text..</p>
+            </div>
+          </div> */}
+        </div>
       </div>
     );
   }
