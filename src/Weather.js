781798e18b6655ca80cdc5a31e63f92e5170b57e
Samiul karim <samiulkarimprodhan@gmail.com>
2021-06-21 20:19:56 +0600

    edited weather search box

--- src/Weather.js
+++ src/Weather.js
@@ -6,22 +6,17 @@ class Weather extends React.Component {
     return (
       <div className="column right weather div only">
         <div className="ui segment right">
-          <div class="ui category search">
-            <div className="ui icon input">
-              <input
-                className="prompt"
-                type="text"
-                placeholder="Search City..."
-              ></input>
-              <i className="search icon"></i>
-            </div>
-          </div>
+        <div className="ui fluid icon input">
+  <input type="text" className= "ui input SearchCity" placeholder="Search City...."></input>
+  <i class="search icon"></i>
+</div>
+          
           <i className="sun icon"></i>
           <h1 className="temparature degree">12</h1>
           <p className="temparature unit">°C</p>
           <p>Mostly Cloudy</p>
           <p>Rain, 30%</p>
-          <div class="ui divider"></div>
+          <div className="ui divider"></div>
           <TimeAndDate></TimeAndDate>
         </div>
       </div>
